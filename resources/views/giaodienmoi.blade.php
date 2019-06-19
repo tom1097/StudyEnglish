99208df55cbcf55bf6419bdd57370da4cefe47f5
tomiot1097 <tomiot1097@gmail.com>
2019-06-19 22:30:46 +0700

    update writing message

--- resources/views/giaodienmoi.blade.php
+++ resources/views/giaodienmoi.blade.php
@@ -89,7 +89,40 @@
                     <h3 class="mt-2">Writing</h3>
                     <div>
                         <p>{!! $video->writing !!}</p>  
-                        <p>Please send us your feed back ideas! <br/>admin@studyenglish.com </p>                   
+                        <p>Please send us your feed back ideas! <br/>admin@studyenglish.com </p>       
+                        
+                        {{--  For modal writing  --}}
+
+                        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+                        Start Writing now
+                        </button>
+
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel">{!! $video->writing !!}</h5>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+              <form>
+                <div class="form-group">
+                  <label for="message-text" class="col-form-label">Your answer:</label>
+                  <textarea rows="6" class="form-control" id="message-text"></textarea>
+                </div>
+              </form>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+              <button type="button" class="btn btn-primary">Send message</button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+                        {{--  End modal Writing  --}}
                     </div>
                 </div>
 
